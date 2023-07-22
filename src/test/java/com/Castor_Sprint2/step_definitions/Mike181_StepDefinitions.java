02aed93eecc0cbf4be98411b68935fe402b557d7
Michael Boone <Mboonetech@gmail.com>
2023-07-21 21:55:50 -0400

    Updated code

--- src/test/java/com/Castor_Sprint2/step_definitions/Mike181_StepDefinitions.java
+++ src/test/java/com/Castor_Sprint2/step_definitions/Mike181_StepDefinitions.java
@@ -13,7 +13,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Mike181_StepDefinitions {
 
@@ -55,8 +59,11 @@ public class Mike181_StepDefinitions {
 
     @And("User should verify the page title is {string}")
     public void VerifyThePageTitleIs(String expectedTitle) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleIs("All - Vehicle Contract - Entities - System - Car - Entities - System"));
         String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
+
+        Assert.assertEquals(expectedTitle,actualTitle);
 
     }
 
